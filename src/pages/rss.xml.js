f70775d0bdbe6fcd9bb76da88241f62c6edede88
Iago Bozza <iagobozza@writeonlycode.io>
2026-05-17 12:51:23 -0300

    Update RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,17 +1,17 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 export async function GET(context) {
-    const site = await getEntry("site", "site");
-    const posts = await getCollection("blog");
+  const site = await getEntry("site", "site");
+  const posts = await getCollection("blog");
 
-    return rss({
-        title: site.title,
-        description: site.description,
-        site: context.site,
-        items: posts.map((post) => ({
-            ...post.data,
-            link: `/blog/${post.id}/`,
-        })),
-    });
+  return rss({
+    title: site.title,
+    description: site.description,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.id}/`,
+    })),
+  });
 }
