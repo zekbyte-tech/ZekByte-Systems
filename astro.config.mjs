cdcc0ccf952cfe172a12cfec47439e23c8b14c66
Iago Bozza <iagobozza@writeonlycode.io>
2026-05-17 13:03:04 -0300

    Update Astro Configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,27 +7,28 @@ import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import { defineConfig, fontProviders } from "astro/config";
 
+const isDev = process.env.NODE_ENV === "development";
+
 export default defineConfig({
-    site: "https://example.com",
-    integrations: [mdx(), sitemap(), icon()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), icon()],
 
-    fonts: [
-        {
-            provider: fontProviders.fontsource(),
-            name: "Geist Sans",
-            cssVariable: "--font-geist",
-        },
-        {
-            provider: fontProviders.fontsource(),
-            name: "Geist Mono",
-            cssVariable: "--font-geist-mono",
-        },
-    ],
-
-    vite: {
-        plugins: [tailwindcss()],
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Geist Sans",
+      cssVariable: "--font-geist",
     },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Geist Mono",
+      cssVariable: "--font-geist-mono",
+    },
+  ],
 
-    // adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: isDev ? undefined : cloudflare(),
 });
-
